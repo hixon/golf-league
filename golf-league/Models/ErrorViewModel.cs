8aa25fd41db252afbc083db01dc193822f6b19bd
hixon <ryan.hixon@gmail.com>
2023-10-11 18:49:54 -0400

    Add project files.

---
+++ golf-league/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace golf_league.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
