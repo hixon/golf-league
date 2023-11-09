2ec02c3ea460f0056d9cd96bdb26153eff5341a2
hixon <ryan.hixon@gmail.com>
2023-11-08 21:05:10 -0500

    add migration for player table

---
+++ golf-league/Migrations/20231109020348_AddPlayerTable.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace golf_league.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPlayerTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlayerType",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    CreateDt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdateDt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartIndex = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    CurrIndex = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    PlayerTypeId = table.Column<long>(type: "bigint", nullable: false),
+                    Active = table.Column<bool>(type: "bit", nullable: false),
+                    CreateDt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdateDt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Player_PlayerType_PlayerTypeId",
+                        column: x => x.PlayerTypeId,
+                        principalTable: "PlayerType",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Player_PlayerTypeId",
+                table: "Player",
+                column: "PlayerTypeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Player");
+
+            migrationBuilder.DropTable(
+                name: "PlayerType");
+        }
+    }
+}
